ee31a31f73105f525cc4b58efc851ce640ce3ee6
Pier <madstrum@gmail.com>
2018-05-25 13:54:17 +0800

    added subject index

--- 2016_img_data_analytics/combine_files.py
+++ 2016_img_data_analytics/combine_files.py
@@ -5,12 +5,15 @@ ctrl_path = "/home/pier/Machine_Learning/Clinical_Health_Parkinsons_Analytics/20
 control_files_list = [f for f in os.listdir(ctrl_path) if os.path.isfile(os.path.join(ctrl_path, f))]
 park_path = "/home/pier/Machine_Learning/Clinical_Health_Parkinsons_Analytics/2016_img_data_analytics/data/parkinsons/"
 parkinson_files_list = [f for f in os.listdir(park_path) if os.path.isfile(os.path.join(park_path, f))]
+subject = 0
 
 with open(ctrl_path + 'control_all.csv', 'w') as outfile:
     for fname in control_files_list:
         fname = ctrl_path + fname
         with open(fname) as infile:
+            subject = subject + 1
             for line in infile:
+                line = str(subject) + "," + line
                 line = line.replace(';', ',')  # make it csv
                 outfile.write(line)
 
@@ -18,16 +21,18 @@ with open(park_path + 'park_all.csv', 'w') as outfile:
     for fname in parkinson_files_list:
         fname = park_path + fname
         with open(fname) as infile:
+            subject = subject + 1
             for line in infile:
+                line = str(subject) + "," + line
                 line = line.replace(';', ',')
                 outfile.write(line)
 
 df = pd.read_csv(ctrl_path + 'control_all.csv')
-df.columns = ['X', 'Y', 'Z', 'Pressure', 'GripAngle', 'Timestamp', 'Test_ID']
+df.columns = ['Subject','X', 'Y', 'Z', 'Pressure', 'GripAngle', 'Timestamp', 'Test_ID']
 df['PWP'] = 0
-df.to_csv(ctrl_path + 'control_all_with_header.csv')
+df.to_csv(ctrl_path + 'control_all_with_header.csv', index=False)
 
 df = pd.read_csv(park_path + 'park_all.csv')
-df.columns = ['X', 'Y', 'Z', 'Pressure', 'GripAngle', 'Timestamp', 'Test_ID']
+df.columns = ['Subject','X', 'Y', 'Z', 'Pressure', 'GripAngle', 'Timestamp', 'Test_ID']
 df['PWP'] = 1
-df.to_csv(park_path + 'park_all_with_header.csv')
+df.to_csv(park_path + 'park_all_with_header.csv', index=False)
